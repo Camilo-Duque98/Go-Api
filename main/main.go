711c495919cac65fb38744353045699c110d4089
Camilo-Villar <camilo.villar@sansano.usm.cl>
2022-09-21 16:02:17 -0300

    falta compras en clientes y ver estadisticas en administrador

--- main/main.go
+++ main/main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type Productos struct {
@@ -18,6 +19,12 @@ type Producto struct {
 	Nombre              string `json:"nombre"`
 	Cantidad_disponible int    `json:"cantidad_disponible"`
 	Precio_unitario     int    `json:"precio_unitario"`
+}
+
+type CreateProduct struct {
+	Nombre   string `json:"nombre"`
+	Cantidad int    `json:"cantidad_disponible"`
+	Precio   int    `json:"precio_unitario"`
 }
 
 type LoginStruct struct {
@@ -51,6 +58,55 @@ func GetProducts() {
 	}
 
 }
+func PostProduct() string {
+
+	var name string
+	var quantity int
+	var price int
+
+	fmt.Print("Ingrese el nombre: ")
+	fmt.Scanln(&name)
+	fmt.Print("Ingrese la disponibilidad: ")
+	fmt.Scanln(&quantity)
+	fmt.Print("Ingrese el precio unitario: ")
+	fmt.Scanln(&price)
+
+	product := CreateProduct{name, quantity, price}
+	jsonReq, err := json.Marshal(product)
+	resp, err := http.Post("http://localhost:8080/api/producto", "application/json; charset=utf-8", bytes.NewBuffer(jsonReq))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	//bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+	//bodyString := string(bodyBytes)
+
+	//fmt.Println(bodyString)
+
+	return "Producto ingresado correctamente"
+}
+func DeleteProduct() {
+	var id_producto int
+	fmt.Print("Ingrese el id del producto a eliminar: ")
+	fmt.Scanln(&id_producto)
+	url := "http://localhost:8080/api/producto/" + strconv.Itoa(id_producto)
+	//jsonReq, err:= json.Marshal(id_producto)
+	req, err := http.NewRequest(http.MethodDelete, url, nil)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+	bodyString := string(bodyBytes)
+	fmt.Println(bodyString)
+}
 func Login() bool {
 	var id int
 	var contrasena string
@@ -59,7 +115,6 @@ func Login() bool {
 	fmt.Scanln(&id)
 	fmt.Print("Ingrese su contraseña: ")
 	fmt.Scanln(&contrasena)
-	//fmt.Println("Probando POST-------")
 
 	login := LoginStruct{id, contrasena}
 	jsonReq, err := json.Marshal(login)
@@ -74,7 +129,6 @@ func Login() bool {
 
 	bodyString := string(bodyBytes)
 	if bodyString == "{\"acceso valido\":true}" {
-		//fmt.Println("Funciona")
 		return true
 	} else {
 		return false
@@ -99,6 +153,46 @@ func ClientOption() {
 		case 3:
 			boolean = false
 		}
+	}
+}
+func ManagerOption() {
+	boolean := true
+
+	fmt.Println("Opciones:")
+	for boolean == true {
+		var option int
+		fmt.Println("1. Ver lista de productos")
+		fmt.Println("2. Crear producto")
+		fmt.Println("3. Eliminar producto")
+		fmt.Println("4. Ver estadísticas")
+		fmt.Println("5. Salir")
+		fmt.Print("Ingrese una opción: ")
+		fmt.Scanln(&option)
+
+		switch option {
+		case 1:
+			GetProducts()
+		case 2:
+			fmt.Println(PostProduct())
+		case 3:
+			DeleteProduct()
+		case 4:
+			fmt.Println("Aquí va a ver estadísticas")
+		case 5:
+			boolean = false
+		default:
+			fmt.Println("Ingrese una opción válida")
+		}
+	}
+}
+
+func ManagerSession() {
+	boolean := Login()
+	if boolean == true {
+		fmt.Println("Inicio de sesión exitoso")
+		ManagerOption()
+	} else {
+		fmt.Println("Error, no hay ninguna coincidencia con los datos ingresados.")
 	}
 }
 
@@ -135,7 +229,7 @@ func main() {
 		case 1:
 			ClientSession()
 		case 2:
-			fmt.Println("Administrador")
+			ManagerSession()
 		case 3:
 			boolean = false
 		default:
@@ -143,28 +237,4 @@ func main() {
 		}
 	}
 	fmt.Println("Hasta luego!")
-
-	/*fmt.Println("Hola mundo!")
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://localhost:8080/api/productos", nil)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	defer resp.Body.Close()
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
-	var responseObject Productos
-
-	json.Unmarshal(bodyBytes, &responseObject)
-	fmt.Println("Nombre del producto: ", responseObject)*/
-
 }
